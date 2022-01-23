0d86246c10377b2ce7296d047a329295da2bd7fc
Marc Fors <marcfs31@gmail.com>
2022-01-23 09:57:52 +0100

    Initial Commit

---
+++ Shared/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  Shared
+//
+//  Created by Marc Fors on 23/1/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
