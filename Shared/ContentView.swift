1964d3331c3c663f23d45dca072fbcd052f22e57
Marc Fors <marcfs31@gmail.com>
2022-01-23 14:58:21 +0100

    Card game done

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0;
+    @State private var cpuScore = 0;
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background").ignoresSafeArea()
+            // Vertical stack
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                // Horizontal stack
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                // Deal button
+                Spacer()
+                Button(action: {
+                    // Randomize cards
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    // Update cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                // Horizontal stack
+                HStack {
+                    Spacer()
+                    // Vertical stack
+                    VStack {
+                        // Text with modifiers for the player
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    // Vertical stack
+                    VStack {
+                        // Text with modifiers for the CPU
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
